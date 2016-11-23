119e7de0174f7f61be7aebd63b9893bfdd61c37b
Sergio Garcia Garcia <sgarcia@cupagroup.com>
2016-11-23 19:01:29 +0100

    Avances en el JS para la validacion de campos y precios

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 // Declarar el objeto para que no falle
-let es = {brujula: {paymentMethods: "1"}};
+let es = { brujula: { paymentMethods: {} }};
+
 
 // Creamos uan interface para asegurarnos que todas las clases que la implementen cumplan los requisitos (No necesario en este ejemplo);
 interface Payment {
@@ -15,25 +16,74 @@ interface Payment {
 class PaymentMethod implements Payment {
     view = {};
     paymentMethodId: string;
+    price: number;
+    discountFee: number = 10;
     isSelected() {
-
+        return document.querySelector("input[type=radio][name='MethodPay'][id=" + this.paymentMethodId + "]").checked;
     };
     getTotalFee() {
-
+        switch (this.paymentMethodId) {
+            case "Efectivo": return 1.5 + (this.price - (this.discountFee * this.price / 100));
+            case "Paypal": return 8.30 + (this.price - (this.discountFee * this.price / 100));
+            case "Tarjeta": return 6.80 + (this.price - (this.discountFee * this.price / 100));
+        }
     };
     getTotalFeeWithoudDiscounts() {
-
+        switch (this.paymentMethodId) {
+            case "Efectivo": return 1.5 + this.price;
+            case "Paypal": return 8.30 + this.price;
+            case "Tarjeta": return 6.80 + this.price;
+        }
     }
     initView(): void {
 
     }
-    constructor(paymentMethodId: string) {
+    constructor(paymentMethodId: string, price: number) {
         this.paymentMethodId = paymentMethodId;
+        this.price = price;
         this.initView();
     }
 }
 
 
+window.onload = () => {
+    try {
+        // Parte de los radios
+        let inputRadio = document.querySelectorAll("input[type=radio][name='MethodPay']");
+        console.log(inputRadio);
+        for (let radio of inputRadio) {
+            radio.addEventListener("change", () => {
+                let price = parseFloat(document.querySelector("input[id='price-input'][type='number']").value);
+                es.brujula.paymentMethods = new PaymentMethod(radio.id, price);
+                console.log(es.brujula.paymentMethods);
+            });
+        }
+    }catch (e) {}
+
+    try {
+        // Parte de los botones
+        let btnTest = document.querySelectorAll("button[name='btn-test']");
+        console.log(btnTest);
+        for (let btn of btnTest) {
+            btn.addEventListener("click", () => {
+                switch (btn.value) {
+                case "isSelected":
+                    console.log('Boton isSelected');
+                    console.log(es.brujula.paymentMethods.isSelected());
+                    break;
+                case "getTotalFee":
+                    console.log('Boton getTotalFee');
+                    console.log(es.brujula.paymentMethods.getTotalFee());
+                    break;
+                case "getTotalFeeWithoudDiscounts":
+                    console.log('Boton getTotalFeeWithoudDiscounts');
+                    console.log(es.brujula.paymentMethods.getTotalFeeWithoudDiscounts());
+                    break;
+                }
+            });
+        }
+    }catch (e) {}
+};
 
 
 
--- js/app.js
+++ js/app.js
@@ -1,22 +1,83 @@
 // Declarar el objeto para que no falle
-var es = { brujula: { paymentMethods: "1" } };
+var es = { brujula: { paymentMethods: {} } };
 // Ahora definimos la clase que utilizara cada instancia de los objetos creados, para los eventos en el DOM sobre metodos de pago.
 var PaymentMethod = (function () {
-    function PaymentMethod(paymentMethodId) {
+    function PaymentMethod(paymentMethodId, price) {
         this.view = {};
+        this.discountFee = 10;
         this.paymentMethodId = paymentMethodId;
+        this.price = price;
         this.initView();
     }
     PaymentMethod.prototype.isSelected = function () {
+        return document.querySelector("input[type=radio][name='MethodPay'][id=" + this.paymentMethodId + "]").checked;
     };
     ;
     PaymentMethod.prototype.getTotalFee = function () {
+        switch (this.paymentMethodId) {
+            case "Efectivo": return 1.5 + (this.price - (this.discountFee * this.price / 100));
+            case "Paypal": return 8.30 + (this.price - (this.discountFee * this.price / 100));
+            case "Tarjeta": return 6.80 + (this.price - (this.discountFee * this.price / 100));
+        }
     };
     ;
     PaymentMethod.prototype.getTotalFeeWithoudDiscounts = function () {
+        switch (this.paymentMethodId) {
+            case "Efectivo": return 1.5 + this.price;
+            case "Paypal": return 8.30 + this.price;
+            case "Tarjeta": return 6.80 + this.price;
+        }
     };
     PaymentMethod.prototype.initView = function () {
     };
     return PaymentMethod;
 }());
+window.onload = function () {
+    try {
+        // Parte de los radios
+        var inputRadio = document.querySelectorAll("input[type=radio][name='MethodPay']");
+        console.log(inputRadio);
+        var _loop_1 = function(radio) {
+            radio.addEventListener("change", function () {
+                var price = parseFloat(document.querySelector("input[id='price-input'][type='number']").value);
+                es.brujula.paymentMethods = new PaymentMethod(radio.id, price);
+                console.log(es.brujula.paymentMethods);
+            });
+        };
+        for (var _i = 0, inputRadio_1 = inputRadio; _i < inputRadio_1.length; _i++) {
+            var radio = inputRadio_1[_i];
+            _loop_1(radio);
+        }
+    }
+    catch (e) { }
+    try {
+        // Parte de los botones
+        var btnTest = document.querySelectorAll("button[name='btn-test']");
+        console.log(btnTest);
+        var _loop_2 = function(btn) {
+            btn.addEventListener("click", function () {
+                switch (btn.value) {
+                    case "isSelected":
+                        console.log('Boton isSelected');
+                        console.log(es.brujula.paymentMethods.isSelected());
+                        break;
+                    case "getTotalFee":
+                        console.log('Boton getTotalFee');
+                        console.log(es.brujula.paymentMethods.getTotalFee());
+                        break;
+                    case "getTotalFeeWithoudDiscounts":
+                        console.log('Boton getTotalFeeWithoudDiscounts');
+                        console.log(es.brujula.paymentMethods.getTotalFeeWithoudDiscounts());
+                        break;
+                }
+            });
+        };
+        for (var _a = 0, btnTest_1 = btnTest; _a < btnTest_1.length; _a++) {
+            var btn = btnTest_1[_a];
+            _loop_2(btn);
+        }
+    }
+    catch (e) { }
+};
+// Animaciones y parte visual del HTML 
 //# sourceMappingURL=app.js.map
